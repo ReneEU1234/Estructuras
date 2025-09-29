342d8cb4d5f9893c9f9f305c0dd653acfe9c8f11
ReneEU1234 <jiatzrene2@gmail.com>
2025-09-29 00:14:53 -0600

    Add files via upload

---
+++ Estructuras.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class botonXD {
+
+    public static void main(String[] args) {
+
+// 1-Array
+        int[] numeros = new int[5];  
+        String[] nombres = {"Ruben", "Donato", "Pichol"};
+
+// 2-Lista
+        String[] frutas = {"Manzana", "Banana", "Fresa"};
+        JList<String> listaFrutas = new JList<>(frutas);
+
+// 3-Pila
+        Stack<Integer> pila = new Stack<>();
+
+// 4-Cola
+        Queue<String> cola = new LinkedList<>();
+
+// 5-Arbol
+        TreeSet<Integer> arbol = new TreeSet<>();
+
+// 6-Grafo
+        Map<String, List<String>> grafo = new HashMap<>();
+
+    }
+}
